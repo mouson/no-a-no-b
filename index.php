8c853984b466f5982b05de19a2ad1d1a0c4fe1e7
Shouko <h999342@gmail.com>
2014-05-04 21:25:01 +0800

    add background selections

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 
-	<title>Project Lunchbox</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="style.css?<?php echo time()?>">
-	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <title>Project Lunchbox</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css?<?php echo time()?>">
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
   <script src="./html2canvas.js?"></script>
   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
   <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
@@ -38,12 +38,13 @@ $.ajax({
 });
 }
 
-	function create(){
+  function create(){
     $("#result-cb").html($("#input-cb").val());
     $("#result-cs").html($("#input-cs").val());
     $("#result-eb").html($("#input-eb").val());
     $("#result-es").html($("#input-es").val());
-		$("#flag-outline").css('visibility', "visible");
+    $("#flag-outline").css('background-image', "url('flag_back/"+$('input[name=input-back]:checked').val()+".png')");
+    $("#flag-outline").css('visibility', "visible");
         html2canvas($("#flag-outline"), {
          onrendered: function(canvas) {
         imgToPost = canvas.toDataURL('image/png');
@@ -54,22 +55,41 @@ $.ajax({
        $("#iuLink").css('display',"none");
         }
        });
-	}
-	</script>
+  }
+  </script>
 </head>
 <body>
-	<div class="creator-form">
-		<h1>Project Lunchbox</h1>
-		<h4>歡迎使用 No A No B 產生器<br>請填入資料即可產生相關圖片 ^.<</h4>
+  <div class="creator-form">
+    <h1>Project Lunchbox</h1>
+    <h4>歡迎使用 No A No B 產生器<br>請填入資料即可產生相關圖片 ^.<</h4>
 
 <div id="data-form">
-		中文大字：<input id="input-cb" placeholder="反呵呵"><br>
-		中文小字：<input id="input-cs" placeholder="不要再有下一個QQQ"><br>
-		英文大字：<input id="input-eb" placeholder="No KEKE"><br>
-		英文小字：<input id="input-es" placeholder="No More KerKerKer"><br>
-		<button onclick="create()" class="go-button">Create</button>
+    中文大字：<input id="input-cb" placeholder="反呵呵"><br>
+    中文小字：<input id="input-cs" placeholder="不要再有下一個QQQ"><br>
+    英文大字：<input id="input-eb" placeholder="No KEKE"><br>
+    英文小字：<input id="input-es" placeholder="No More KerKerKer"><br>
+    選擇背景：
+    <table>
+      <tr>
+        <td><label><input type="radio" name="input-back" value="taiwan" checked />台灣</label></td>
+        <td><label><input type="radio" name="input-back" value="cow" />牛</label></td>
+        <td><label><input type="radio" name="input-back" value="dog" />狗</label></td>
+        <td><label><input type="radio" name="input-back" value="fish" />魚</label></td>
+        <td><label><input type="radio" name="input-back" value="jet" />戰機</label></td>
+        <td><label><input type="radio" name="input-back" value="kuma" />熊</label></td>
+      </tr>
+      <tr>
+        <td><img src="flag_back/taiwan.png" class="thumb" /></td>
+        <td><img src="flag_back/cow.png" class="thumb" /></td>
+        <td><img src="flag_back/dog.png" class="thumb" /></td>
+        <td><img src="flag_back/fish.png" class="thumb" /></td>
+        <td><img src="flag_back/jet.png" class="thumb" /></td>
+        <td><img src="flag_back/kuma.png" class="thumb" /></td>
+      </tr>
+    </table>
+    <button onclick="create()" class="go-button">Create</button>
 </div>
-	</div>
+  </div>
 
    <div id="canvas-container"></div>
 
@@ -77,15 +97,27 @@ $.ajax({
      <button onclick="uploadToImgur()" class="post-button">上傳到ImgUR</button><br><br>
      <input id="iuLink" size="30" onclick="this.setSelectionRange(0, this.value.length)" style="display: none" />
    </div>
-	<div id="flag-outline">
-		<ul class="flag-info">
-			<span id="result-cb" class="results">反呵呵</span>
-			<span id="result-cs" class="results">不要再有下一個QQQ</span>
-			<span id="result-eb" class="results">No KEKE</span>
-			<span id="result-es" class="results">No More KerKerKer</span>
-		</ul>
-	</div>
+  <div id="flag-outline" style="background-image: url('flag_back/taiwan.png');">
+    <ul class="flag-info">
+      <span id="result-cb" class="results">反呵呵</span>
+      <span id="result-cs" class="results">不要再有下一個QQQ</span>
+      <span id="result-eb" class="results">No KEKE</span>
+      <span id="result-es" class="results">No More KerKerKer</span>
+    </ul>
+  </div>
 
-	<div id="footer">by Shouko</div>
+  <div id="footer">by Shouko</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45786123-3', 'shouko.tw');
+  ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
